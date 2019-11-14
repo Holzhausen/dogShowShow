398943452067e7e1e8e8618bcff592f12a713394
Holzhausen <holzhausen@outlook.com>
2019-11-14 09:38:00 -0300

    Updates

---
+++ precache-manifest.d27bcc9131c1783a68f3380a04fdfc31.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89754f5d0ac555a0db5641254688f402",
+    "url": "/dogShowShow/index.html"
+  },
+  {
+    "revision": "f924fd9eef9560bc7fad",
+    "url": "/dogShowShow/static/css/main.d44b3f88.chunk.css"
+  },
+  {
+    "revision": "46c1866cc51c28e4bffd",
+    "url": "/dogShowShow/static/js/2.c19cb2da.chunk.js"
+  },
+  {
+    "revision": "f924fd9eef9560bc7fad",
+    "url": "/dogShowShow/static/js/main.b98c173d.chunk.js"
+  },
+  {
+    "revision": "5452273b674f41e712cc",
+    "url": "/dogShowShow/static/js/runtime-main.8635edce.js"
+  },
+  {
+    "revision": "c7ea37b1332eb3de9f29eaaf48152516",
+    "url": "/dogShowShow/static/media/Eczar-Regular.c7ea37b1.ttf"
+  },
+  {
+    "revision": "6c57263dbdee325394e471a12499e64c",
+    "url": "/dogShowShow/static/media/Heebo-Medium.6c57263d.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/dogShowShow/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/dogShowShow/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "8affd2480f4fa970017b364a451dfc35",
+    "url": "/dogShowShow/static/media/SupermercadoOne-Regular.8affd248.ttf"
+  }
+]);
